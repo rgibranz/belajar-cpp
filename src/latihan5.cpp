a58d429f8da912971d14796a74c35eafbe431082
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-03-24 17:03:43 +0700

    operator aritmatika

---
+++ src/latihan5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // operatornya +, - , *, /, % (tambah, kurang, kali, bagi, modulus)
+
+    // penjumlahan
+    int a = 1 + 2;
+    cout << "hasilnya " << a << endl;
+
+    // pengurangan
+    int b = 2 - 1;
+    cout << "hasilnya " << b << endl;
+
+    // pengalian
+    int c = 1 * 2;
+    cout << "hasilnya " << c << endl;
+
+    // pembagian
+    int d = 4 / 2;
+    cout << "hasilnya " << d << endl;
+
+    // sisa bagi (modulus)
+    int e = 8 % 3;
+    cout << "hasilnya " << e << endl;
+
+    return 0;
+}
