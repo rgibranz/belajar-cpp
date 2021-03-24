9b2c2c00cdaf92211ad16423fb7175f5f2042f9c
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-03-24 21:04:28 +0700

    assignment

---
+++ src/latihan12.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a = 12;
+    cout << "nilai awal adalah " << a << endl;
+
+    // ditambah
+    a += 2;
+    cout << "ditambah 2 : " << a << endl;
+
+    //dikurang
+    a -= 4;
+    cout << "dikurang 4 : " << a << endl;
+
+    //dikali
+    a *= 2;
+    cout << "dikali 2 :" << a << endl;
+
+    //dibagi
+    a /= 5;
+    cout << "dibagi 5 : " << a << endl;
+
+
+    return 0;
+}
