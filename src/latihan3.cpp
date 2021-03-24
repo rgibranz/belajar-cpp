2a01ad504909d1a8aaa85df1a0f4aa6017078133
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-03-24 15:16:24 +0700

    deklarasi dan cin

---
+++ src/latihan3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a; //hanya deklarasi variabel
+    a = 12; // mengisi variabel nya
+    cout << a << endl;
+
+    cout << "masukan angka : ";
+    cin >> a; //memasukan kedalam variabel
+    cout << "Nilai Yang Anda Masukan : " << a;
+
+    return 0;
+}
