8a708d522633f63041ae353f5d21cf2d3b8abd52
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-03-24 14:49:56 +0700

    printing pada console

---
+++ src/latihan1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Tanpa Garis Baru";
+    std::cout << "Membuat Baris Baru \n";
+    std::cout << "Halo Gibran. Membuat Baris Baru dengan std endl" << std::endl;
+    std::cin.get();
+    return 0;
+}
