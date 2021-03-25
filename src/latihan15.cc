1bb01e84bbdf44451a1b96a7e35db636741648ff
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-03-25 09:17:43 +0700

    do while

---
+++ src/latihan15.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    do
+    {
+        cout << "nilai a : " << a << endl;
+        a += 1;
+    } while (a < 20);
+
+    return 0;
+}
