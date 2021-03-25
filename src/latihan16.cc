1c090e4ce6d6abedc2b552c9c1dfa82614b36fac
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-03-25 09:50:20 +0700

    for loop

---
+++ src/latihan16.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    for( int a = 1 ; a > 0; a += a){
+
+        cout << "awokaokaow ke " << a << endl;
+    
+    }
+
+    return 0;
+}
